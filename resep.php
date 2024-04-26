62f65d0c7870353b63f1fbd86fddec7f11b152ee
bimantaraaryasatya <bimantara_aryasatya_32rpl@student.smktelkom-mlg.sch.id>
2024-04-26 10:04:16 +0700

    second

--- resep.php
+++ resep.php
@@ -103,7 +103,7 @@
             </div>
 
             <div class="tutorial-video">
-            <iframe width="460" height="315" src="https://www.youtube.com/embed/EHQpyfEzQ24?si=qw8763j3zsHXj46l" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe width="460" height="315" src="https://www.youtube.com/embed/EHQpyfEzQ24?si=qw8763j3zsHXj46l" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
         </div>
     </section>
